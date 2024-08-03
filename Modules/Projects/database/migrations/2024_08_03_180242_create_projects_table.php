f4e95996343efb840119a4ded751aaf369c47125
Youssef548 <youssef@youssef>
2024-08-03 22:07:57 +0300

    feat: create projects table migration

---
+++ Modules/Projects/database/migrations/2024_08_03_180242_create_projects_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('projects', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 100);
+            $table->integer('teams_id');
+            $table->timestamps();
+
+            
+            // Uncomment this line once the teams table is created
+            // $table->foreign('teams_id')->references('id')->on('teams')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('projects');
+    }
+};
