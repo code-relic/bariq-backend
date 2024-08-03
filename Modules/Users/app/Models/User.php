fe0c37cca0ed64c53353f4853c66bc2368074c79
mohamedhoss123 <81392900+mohamedhoss123@users.noreply.github.com>
2024-08-03 17:15:34 +0300

    fix:change-auth-model
    Change auth model from old on at app folder to new one under Users module

--- Modules/Users/app/Models/User.php
+++ Modules/Users/app/Models/User.php
@@ -1,8 +1,7 @@
 <?php
 
-namespace App\Models;
+namespace Modules\Users\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
