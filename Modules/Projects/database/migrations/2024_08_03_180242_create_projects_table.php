b88b0594e5288c8e231ded4942afb5f14fc699ab
Abdulkhalek-1 <abdulkhalek.muhammad.work@gmail.com>
2024-08-04 09:47:00 +0300

    UnComment: teams_id forgin key

--- Modules/Projects/database/migrations/2024_08_03_180242_create_projects_table.php
+++ Modules/Projects/database/migrations/2024_08_03_180242_create_projects_table.php
@@ -14,12 +14,11 @@ return new class extends Migration
         Schema::create('projects', function (Blueprint $table) {
             $table->id();
             $table->string('name', 100);
-            $table->integer('teams_id');
+            $table->unsignedBigInteger('teams_id');
             $table->timestamps();
 
-            
-            // Uncomment this line once the teams table is created
-            // $table->foreign('teams_id')->references('id')->on('teams')->onDelete('cascade');
+            //? ForginKeys
+            $table->foreign('teams_id')->references('id')->on('teams')->onDelete('cascade');
         });
     }
 
