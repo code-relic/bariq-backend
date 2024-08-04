bbb822825b057f8ea9b0037915393f4f7cd54d52
Abdulkhalek-1 <abdulkhalek.muhammad.work@gmail.com>
2024-08-04 09:39:14 +0300

    Change: team_id & user_id to unsignedBigInteger in team_users Migration

--- Modules/Teams/database/migrations/2024_08_03_145931_create_team_users_table.php
+++ Modules/Teams/database/migrations/2024_08_03_145931_create_team_users_table.php
@@ -13,8 +13,8 @@ return new class extends Migration
     {
         Schema::create('team_users', function (Blueprint $table) {
             $table->id();
-            $table->integer('team_id');
-            $table->integer('user_id');
+            $table->unsignedBigInteger('team_id');
+            $table->unsignedBigInteger('user_id');
             $table->timestamps();
 
             //? Foreign Key
