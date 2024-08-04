102d1eba72e4554bb6c6cb052917ff43bce4188f
Abdulkhalek-1 <abdulkhalek.muhammad.work@gmail.com>
2024-08-04 09:39:41 +0300

    Fix: dropColumns in down() into update user migration

--- Modules/Users/database/migrations/2024_08_03_102146_update_user_table.php
+++ Modules/Users/database/migrations/2024_08_03_102146_update_user_table.php
@@ -21,6 +21,8 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropColumns('is_2fa_enabled');
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('is_2fa_enabled');
+        });
     }
 };
