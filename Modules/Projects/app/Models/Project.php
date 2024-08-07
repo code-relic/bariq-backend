f993c3325868cfc2f7e316acc914f0505d984995
Youssef548 <youssef@youssef>
2024-08-08 01:33:40 +0300

    feat: Add Projects model and its relationship with Team

---
+++ Modules/Projects/app/Models/Project.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Modules\Projects\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Modules\Projects\Database\Factories\ProjectsFactory;
+
+class Project extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'teams_id',
+    ];
+
+    public function team()
+    {
+        return $this->belongsTo('Modules\Team\Entities\Team');
+    }
+}
