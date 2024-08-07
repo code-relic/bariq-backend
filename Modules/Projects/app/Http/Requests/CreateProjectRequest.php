2ff0d0d8efe70938b63e285380c6470c3b672041
Youssef548 <youssef@youssef>
2024-08-08 01:34:16 +0300

    feat: Add CreateProjectRequest for project creation validation

---
+++ Modules/Projects/app/Http/Requests/CreateProjectRequest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Modules\Projects\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class CreateProjectRequest extends FormRequest
+{
+    /**
+     * Get the validation rules that apply to the request.
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => 'required|string|max:100',
+            'teams_id' => 'required|integer|exists:teams,id',
+        ];
+    }
+
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+}
