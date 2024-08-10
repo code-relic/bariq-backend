8b746e0e6e26656adcff9897d6611aacd84f1939
Abdulkhalek-1 <abdulkhalek.muhammad.work@gmail.com>
2024-08-10 12:49:03 +0300

    refactor(config): adjust URL scheme based on environment
    - Updated `boot` method to force `http` for localhost and `127.0.0.1`, and `https` for other environments.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +20,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        if (in_array(request()->getHost(), ['localhost', '127.0.0.1'])) {
+            URL::forceScheme('http');
+        } else {
+            URL::forceScheme('https');
+        }
     }
 }
